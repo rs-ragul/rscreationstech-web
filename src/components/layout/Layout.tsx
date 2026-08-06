b9ce5831d52c2ead5dc5ce93f28d337f943a1c8d
rs-ragul <ragulethicalhacker@gmail.com>
2026-08-06 14:26:28 +0530

    Changed the background to match with glass morph

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from "react";
 import { Navbar } from "./Navbar";
 import { Footer } from "./Footer";
+import { Background } from "./Background";
 
 interface LayoutProps {
   children: ReactNode;
@@ -8,10 +9,13 @@ interface LayoutProps {
 
 export function Layout({ children }: LayoutProps) {
   return (
-    <div className="min-h-screen flex flex-col noise-overlay">
-      <Navbar />
-      <main className="flex-1 pt-16">{children}</main>
-      <Footer />
+    <div className="min-h-screen flex flex-col relative">
+      <Background />
+      <div className="relative z-10 flex flex-col min-h-screen">
+        <Navbar />
+        <main className="flex-1 pt-16">{children}</main>
+        <Footer />
+      </div>
     </div>
   );
 }
